7e43a0d3eaccc58727d81c4ee6c9ae2a145067bb
Baleskin Vitalii <baleskin1@gmail.com>
2021-10-20 17:55:06 +0300

    PEP8
    Modified the solution for better compliance with PEP8 (pylint)
    
    Signed-off-by: Baleskin Vitalii <baleskin1@gmail.com>

--- solution.py
+++ solution.py
@@ -1,14 +1,86 @@
 """
-solution.py file meant to solve the test for position of Software Development Intern (DevOps) (Mechanical Analysis Division, R&D, Siemens PLM Software)
+solution.py file meant to solve the test for position of
+Software Development Intern (DevOps) (Mechanical Analysis Division, R&D, Siemens PLM Software)
 
-:author Baleskin Vitalii 
+:author Baleskin Vitalii
 """
 
+from io import TextIOWrapper
 import os
 import re
+from typing import Tuple
+
+def first_test(folders: set[str], report: TextIOWrapper)->bool:
+    """
+    ft_run and ft_reference directories should both exist
+    :param folders - the list of subfolders in the test folder
+    :param report - the report file for the test
+    """
+    flag_fail = False
+    if "ft_reference" not in folders:
+        report.write("FAIL\n")
+        report.write("directory missing: ft_reference\n")
+        flag_fail = True
+
+    if "ft_run" not in folders:
+        if not flag_fail:
+            report.write("FAIL\n")
+        report.write("directory missing: ft_run\n")
+        flag_fail = True
+    return flag_fail
 
 
-def process_lines(lines):
+def second_test(path: str, report: TextIOWrapper)->Tuple[bool, set[str]]:
+    """
+    *.stdout file and folder names are the same (as set equality)
+    :param path - path to the test folder
+    :param report - the report file for the test
+    """
+    reference_folder = path + "/ft_reference"
+    run_folder = path + "/ft_run"
+    reference_subfolders = set(os.listdir(reference_folder))
+    run_subfolders = set(os.listdir(run_folder))
+    present_folders = run_subfolders.intersection(reference_subfolders)
+    missing_str = ""
+    extra_str = ""
+    if run_subfolders != reference_subfolders:
+        for miss in reference_subfolders.difference(run_subfolders):
+            missing_str+="'"+miss+'/'+miss+".stdout' "
+
+        for ex in run_subfolders.difference(reference_subfolders):
+            for file in os.listdir(run_folder + "/" + ex):
+                if file.endswith(".stdout"):
+                    extra_str+="'" + ex+'/'+file+"' "
+
+    for present_folder in present_folders:
+        folders = {
+            "ref": reference_folder + "/" + present_folder,
+            "run": run_folder + "/" + present_folder
+            }
+        for mis in set(os.listdir(folders["ref"])).difference(set(os.listdir(folders["run"]))):
+            missing_str+="'" + present_folder + "/" + mis + "' "
+
+        for ex in set(os.listdir(folders["run"])).difference(set(os.listdir(folders["ref"]))):
+            if ex.endswith(".stdout"):
+                extra_str += "'" + present_folder + "/" + ex + "' "
+
+    if missing_str != "":
+        missing_str = "In ft_run there are missing files present in ft_reference: " + missing_str
+
+    if extra_str != "":
+        extra_str = "In ft_run there are extra files not present in ft_reference: " + extra_str
+
+    if missing_str != "" or extra_str != "":
+        report.write("FAIL\n" + missing_str + ("\n" if missing_str != "" else "") + extra_str)
+
+    return missing_str != "" or extra_str != "", present_folders
+
+
+def process_lines(lines: list[str])->dict:
+    """
+    processes a set of lines for bricks total and memory working set peak
+    :param lines: an array of lines
+    """
     flag_bricks = True
     flag_wsp = True
     total = None
@@ -18,176 +90,109 @@ def process_lines(lines):
             parsed = re.match(r'MESH::Bricks: Total=(?P<total>.*) Gas=*', line)
             total = int(parsed.group('total'))
             flag_bricks = False
-        
+
         if flag_wsp and line.startswith("Memory Working Set Current"):
-            parsed = re.match(r'Memory Working Set Current = [\d.]* Mb, Memory Working Set Peak = (?P<wsp>.*) Mb', line)
+            parsed = re.match(
+                r'Memory Working Set Current = [\d.]* Mb, Memory Working Set Peak = (?P<wsp>.*) Mb',
+                line)
             #flag_wsp = False  # uncomment to get results similar to the reference file
             if float(parsed.group('wsp')) > max_wsp:
                 max_wsp = float(parsed.group('wsp'))
-    return total, max_wsp
+    return {"total":total, "wsp":max_wsp}
 
 
-def process_file(path):
-    with open(path, "r") as ref:
+def process_file(path: str)->dict:
+    """
+    processes a file for bricks total and memory working set peak
+    :param path - path to the file to be processed
+    """
+    with open(path, "r", encoding="utf-8") as ref:
         lines = ref.readlines()
         return process_lines(lines)
-        
-
-def first_test(folders, report):
-    flag_fail = False
-    if "ft_reference" not in folders:
-        report.write("FAIL\n")
-        report.write("directory missing: ft_reference\n")
-        flag_fail = True
-        
-    if "ft_run" not in folders:
-        if not flag_fail:
-            report.write("FAIL\n")
-        report.write("directory missing: ft_run\n")
-        flag_fail = True
-    return flag_fail
 
 
-def second_test(path, report):
-    reference_folder = path + "/ft_reference"
-    run_folder = path + "/ft_run"
-    reference_subfolders = set(os.listdir(reference_folder))
-    run_subfolders = set(os.listdir(run_folder))
-    extra = set()
-    missing = set()
-    any_extra = False
-    any_missing = False
-    missing_str = ""
-    extra_str = ""
-    if (run_subfolders != reference_subfolders):
-        extra = run_subfolders.difference(reference_subfolders)
-        missing = reference_subfolders.difference(run_subfolders)
-        if len(missing):
-            missing_str = "In ft_run there are missing files present in ft_reference: "
-            any_missing = True
-            for m in missing:
-                missing_str+="'"+m+'/'+m+".stdout' "
-        
-        if len(extra):
-            for e in extra:
-                files = os.listdir(run_folder + "/" + e)
-                if len(files):
-                    for file in files:
-                        if file.endswith(".stdout"):
-                            if not any_extra:
-                                extra_str = "In ft_run there are extra files not present in ft_reference: "
-                                any_extra = True
-                            extra_str+="'" + e+'/'+file+"' "
-    
-    present_folders = run_subfolders.intersection(reference_subfolders)
-    for pf in present_folders:
-        pref = reference_folder + "/" + pf
-        prun = run_folder + "/" + pf
-        run_files = set(os.listdir(prun))
-        ref_files = set(os.listdir(pref))
-        extra_files = run_files.difference(ref_files)
-        missing_files = ref_files.difference(run_files)
-        if len(missing_files):
-            if not any_missing:
-                missing_str = "In ft_run there are missing files present in ft_reference: "
-                any_missing = True
-            for m in missing_files:
-                missing_str+="'" + pf + "/" + m + "' "
-        
-        if len(extra_files):
-            for e in extra_files:
-                if e.endswith(".stdout"):
-                    if not any_extra:
-                        extra_str = "In ft_run there are extra files not present in ft_reference: "
-                        any_extra = True
-                    extra_str += "'" + pf + "/" + e + "' "
-    
-    if any_missing or any_extra:
-        report.write("FAIL\n" + missing_str + ("\n" if len(missing_str) else "") + extra_str)
-        
-    return any_missing or any_extra, present_folders
-
-
-def third_and_fourth_tests(path, present_folders, report):
-    reference_folder = path + "/ft_reference"
-    run_folder = path + "/ft_run"
-    present_files = []
-    for pf in present_folders:
-        filepath = pf + "/" + pf + ".stdout"
-        present_files.append(filepath)
-    
-    present_files = sorted(present_files)
-    test_fail = False
+def third_and_fourth_tests(path: str, present_folders: set[str], report: TextIOWrapper)->bool:
+    """
+    third test: in files check for abssence of lines containing "error"
+                and presence of "Solver finished at" line
+    fourth test: a) find final total number of bricks in reference and run files,
+                    check that the relative run/ref difference < 0.1
+                 b) relative run/ref difference of (max per file) working set peak
+                    memory should be < 0.5
+    NOTE: in the reference output, the search for working set peak value is done the same way
+          as the total number of bricks - the last such line is taken as a source.
+          In the task, however, the maximum working set peak value is to be found
+    :param path - path to the test folder
+    :param present_folders - folders, present in the ft_run and ft_reference directories
+    :param report - the report file for the test
+    """
     output = ""
-    for pf in present_files:
-        run_path = run_folder + "/" + pf
-        reference_path = reference_folder + "/" + pf
-        with open(run_path) as run:
+    for present_file in sorted(
+        [present_folder + "/" + present_folder + ".stdout" for present_folder in present_folders]
+    ):
+        with open(path + "/ft_run/" + present_file, "r", encoding="utf-8") as run:
             lines = run.readlines()
-            flag_solver = False
-            flag_fail = False
             err_lines = ""
-            solver_line = ""
+            solver_line = present_file + ":  missing 'Solver finished at'\n"
             for i,line in enumerate(lines):
                 if "error" in line.lower().replace(":", " ").split():
-                    flag_fail = True
-                    err_lines += pf + "(" + str(i+1) + "): " + line
+                    err_lines += present_file + "(" + str(i+1) + "): " + line
                 if line.startswith("Solver finished at"):
-                    flag_solver = True
-            
-            if not flag_solver:
-                solver_line = pf + ":  missing 'Solver finished at'\n"
-            
-            total, wsp = process_lines(lines)
-            ref_total, ref_wsp = process_file(reference_path)
+                    solver_line = ""
+
+            run_stats = process_lines(lines)
+            ref_stats = process_file(path + "/ft_reference/" + present_file)
             wsp_line = ''
             total_line = ''
-            if abs(wsp/ref_wsp - 1) >= 0.5:
-                flag_fail = True
-                wsp_line += pf + ": different 'Memory Working Set Peak' (ft_run=" + str(wsp) + ", ft_reference=" + str(ref_wsp) + (", rel.diff=%.2f"%(round((wsp/ref_wsp - 1), 2))) + ", criterion=0.5)\n"
-            
-            if abs(total/ref_total - 1) >= 0.1:
-                flag_fail = True
-                total_line += pf + ": different 'Total' of bricks (ft_run=" + str(total) + ", ft_reference=" + str(ref_total) + (", rel.diff=%.2f"%(round((total/ref_total - 1), 2))) + ", criterion=0.1)\n"
+            if abs(run_stats["wsp"]/ref_stats["wsp"] - 1) >= 0.5:
+                wsp_line += present_file + ": different 'Memory Working Set Peak' (ft_run="
+                wsp_line += str(run_stats["wsp"]) + ", ft_reference=" + str(ref_stats["wsp"])
+                wsp_line += f", rel.diff={(round(run_stats['wsp']/ref_stats['wsp'] - 1, 2)):.2f}"
+                wsp_line += ", criterion=0.5)\n"
 
-            if not flag_solver or flag_fail:
+            if abs(run_stats['total']/ref_stats['total'] - 1) >= 0.1:
+                total_line += present_file + ": different 'Total' of bricks (ft_run="
+                total_line += str(run_stats['total']) + ", ft_reference=" + str(ref_stats['total'])
+                total_line += ", rel.diff="
+                total_line += f"{(round((run_stats['total']/ref_stats['total'] - 1), 2)):.2f}"
+                total_line += ", criterion=0.1)\n"
+
+            if solver_line != "" or err_lines != "" or wsp_line != "" or total_line != "":
                 output += err_lines + wsp_line + total_line + solver_line
-                test_fail = True
-    
-    if test_fail:
+
+    if output != "":
         report.write("FAIL\n")
         report.write(output)
-    
-    return test_fail
+
+    return output != ""
 
 
-def generate_report(path):
-    with open(path+"/"+"report.txt", "w") as report:
+def generate_report(path: str):
+    """
+    Generate the report for a certain test.
+    The generation cosists of conducting 4 tests as per the task.
+    :param path - the path to the test folder
+    """
+    with open(path+"/"+"report.txt", "w", encoding="utf-8") as report:
         folders = os.listdir(path)
-        #first test: ft_run and ft_reference should both exist
-        flag_fail = first_test(folders, report)
-        if flag_fail:
+        #first test: ft_run and ft_reference directories should both exist
+        if first_test(folders, report):
             return
         # first test passed
-        # second test: *.stdout file and folder names are the same (as sets)
-        second_result, present_folders = second_test(path, report)
-        if second_result:
+        flag_fail, present_folders = second_test(path, report)
+        if flag_fail:
             return
         # second test passed
-        # third test: in files check for abssence of lines containing "error" and presence of "Solver finished at" line
-        # fourth test: a) find final total number of bricks in reference and run files, check that the relative run/ref difference < 0.1
-        #              b) relative run/ref difference of (max per file) working set peak memory should be < 0.5
-        # NOTE: in the reference output, the search for working set peak value is done the same way 
-        # as the total number of bricks - the last such line is taken as a source.
-        # In the task, however, the maximum working set peak value is to be found
-        failed_34 = third_and_fourth_tests(path, present_folders, report)
-        #third test passed
-        #fourth test passed
-        if not failed_34:
+        if not third_and_fourth_tests(path, present_folders, report):
+            #third and fourth tests passed
             report.write("OK\n")
 
 
 def main():
+    """
+    main function of the solution
+    processes the test sets
+    """
     test_sets = os.listdir("./logs")
     for test_set in test_sets:
         tests = os.listdir("./logs/"+test_set)
@@ -197,7 +202,7 @@ def main():
 
         for test in tests:
             path = "./logs/"+test_set+"/"+test
-            with open(path+"/"+"report.txt", "r") as report:
+            with open(path+"/"+"report.txt", "r", encoding="utf-8") as report:
                 data = report.readlines()
                 data[-1] = data[-1].removesuffix('\n')
                 verdict = data[0].removesuffix('\n')
